21abeba12be39e9423a09ced72d08e57fbb8230b
gyungwonkim <juykim98@naver.com>
2021-09-03 13:11:01 +0900

    210903 [경원] 문서 : 프로젝트 재배포5
    프로젝트 재배포

---
+++ bucketKurly/src/main/java/bucket/kurly/controller/MainController.java
@@ -0,0 +1,18 @@
+package bucket.kurly.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/main.do")
+	public String main() {
+		return "main";
+	}
+	
+	@RequestMapping("login.do")
+	public String login() {
+		return "login";
+	}
+}
