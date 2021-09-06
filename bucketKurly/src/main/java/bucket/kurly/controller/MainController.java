e62c5bfc10783fbec28072a423d7bde63994bea1
Sangwon <lswon9212@gmail.com>
2021-09-06 10:11:40 +0900

    210906 [상원] 문서 추가 : 공지사항 추가
    MainController.java - @Mapping추가(notice.do, notice_no.co)
    notice.jsp - 고객센터 공지사항 화면입니다.
    notice.css - 고객센터 공지사항 스타일시트입니다.
    notice_no.jsp - 공지사항 게시글 상세페이지입니다.
    notice_no.css - 공지사항 게시글 상세페이지 스타일시트입니다.
    
    공지사항 상세페이지css의 경우 공통부분인 Head와 footer를 생략하고 main영역만 적용된 상태입니다.

--- bucketKurly/src/main/java/bucket/kurly/controller/MainController.java
+++ bucketKurly/src/main/java/bucket/kurly/controller/MainController.java
@@ -15,4 +15,16 @@ public class MainController {
 	public String login() {
 		return "login";
 	}
+	
+	//공지사항
+	@RequestMapping("/notice.do")
+	public String notice() {
+		return "notice";
+	}
+	
+	//공지사항 게시글 상세보기
+	@RequestMapping("/notice_no.do")
+	public String notice_no() {
+		return "notice_no";
+	}
 }
